d3c5569b718842966926be7d219fbf48be83017f
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-26 22:06:59 +0800

    [React-Counter] [Raymond] Feature: Added CounterGroup.js

---
+++ src/components/CounterGroup.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Counter from "./Counter";
+
+class CounterGroup extends Component {
+    constructor(props){
+        super(props);
+        this.state= { 
+            total:0,
+        };
+    }
+    
+    initArray(size){
+        const number = size.length > 0 ? parseInt(size) : 0;
+        return Array.from(Array(number).keys());
+    }
+    onChangeHandler = (number) => {
+        
+        let total = this.state.total;
+        this.setState((prevState) => ({total: prevState.total + number}), () => this.props.getTotal(this.state.total));
+        console.log(total);
+    }
+    render() {
+       const size = this.props.size;
+       const counterSizeArray = this.initArray(size);
+        return (
+            <div>
+                {
+                    counterSizeArray.map((value) => (
+                        <Counter key={value} onChangeHandler={this.onChangeHandler}/>
+                    ))
+                }
+                
+            </div>
+        );
+    }
+}
+
+export default CounterGroup;
